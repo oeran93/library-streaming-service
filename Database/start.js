124422597c737f9780635864c5c078b08256d0b9
System Administrator <root@guild.dhcp.wustl.edu>
2017-02-13 11:28:53 -0600

    fixing indentation

--- Database/start.js
+++ Database/start.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 const env = process.env
 
 module.exports = function () {
-	const auth = `mongodb://${env.LSS_DB_USERNAME}:${env.LSS_DB_PASSWORD}@localhost/${env.LSS_DB_NAME}`
-	mongoose.connect(auth,
-		(err, res) => {
-			if (err) {
-				console.log(err.message);
-			}
-		}
-	)	
+  const auth = `mongodb://${env.LSS_DB_USERNAME}:${env.LSS_DB_PASSWORD}@localhost/${env.LSS_DB_NAME}`
+  mongoose.connect(auth,
+    (err, res) => {
+      if (err) {
+        console.log(err.message);
+      }
+    }
+  )
 }
